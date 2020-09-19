eff6463b13912e86e3fff5b0646c44570e3b1afa
Raoul DIFFOUO <diraulo@gmail.com>
2020-09-19 12:43:19 +0200

    specs: fix them specs, will you :smirk:

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders greeting message", () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const text = getByText(/Hi there/i);
+  expect(text).toBeInTheDocument();
 });
